40e26396be79beda317c5dfd6d8d757e31c701ba
dev4ult <nibrasbiasaaja@gmail.com>
2022-12-04 15:42:57 +0700

    add regisration page

---
+++ app/views/registration/index.php
@@ -0,0 +1,32 @@
+<div class="hero min-h-screen bg-base-200">
+    <div class="hero-content flex-col">
+        <div class="text-center ">
+            <h1 class="text-5xl font-bold">Register now!</h1>
+            <p class="py-6 max-w-3xl">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+                exercitationem
+                quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+        </div>
+        <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div class="card-body">
+                <div class="form-control">
+                    <label class="label">
+                        <span class="label-text">Email</span>
+                    </label>
+                    <input type="text" placeholder="email" class="input input-bordered" />
+                </div>
+                <div class="form-control">
+                    <label class="label">
+                        <span class="label-text">Password</span>
+                    </label>
+                    <input type="text" placeholder="password" class="input input-bordered" />
+                    <label class="label">
+                        <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
+                    </label>
+                </div>
+                <div class="form-control mt-6">
+                    <button class="btn btn-primary">Register</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
