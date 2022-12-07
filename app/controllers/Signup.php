6b70f837843d066c28e3b3bd4b18d29e84c151f0
dev4ult <nibrasbiasaaja@gmail.com>
2022-12-07 20:13:11 +0700

    user registration as feature

--- app/controllers/Signup.php
+++ app/controllers/Signup.php
@@ -1,19 +1,24 @@
 <?php
 
-class Registration extends Controller {
+class Signup extends Controller {
     public function index() {
-        $data['page-title'] = "Registration Page";
+        $data['page-title'] = "Signup Page";
 
         $this->view('templates/header', $data);
         $this->view('templates/navbar/main-navbar');
-        $this->view('registration/index');
+        $this->view('signup/index');
         $this->view('templates/footer');
     }
 
     public function add() {
-        if ($this->model('User_model')->addNewUser($_POST) > 0) {
+        if ($this->model('User_model')->checkExistEmail($_POST['email']) == false) {
+            if ($this->model('User_model')->addNewUser($_POST) > 0) {
+                echo "<script>alert('Your request has been sent')</script>";
+            }
+        } else {
+            echo "<script>alert('That email has been sending a new user request')</script>";
         }
-        header('Location: signup.php');
+        header('Location: ' . BASEURL . '/signup');
         exit;
     }
 }
