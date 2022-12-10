5a4def2d3dce9936ab6f84bb726a174adf75f3de
dev4ult <nibrasbiasaaja@gmail.com>
2022-12-10 22:03:36 +0700

    add conditional session after login

--- app/controllers/Dashboard.php
+++ app/controllers/Dashboard.php
@@ -2,9 +2,23 @@
 
 class Dashboard extends Controller {
     public function index() {
-        $data['page-title'] = 'Dashboard page';
-        $this->view('templates/header');
-        $this->view('templates/navbar/main-navbar');
-        $this->view('templates/footer');
+        if (!isset($_SESSION['login'])) {
+            $_SESSION['login'] = false;
+            header('Location: ' . BASEURL . '/login');
+            exit;
+        } else {
+            if ($_SESSION['login']) {
+
+                $data['page-title'] = 'Dashboard page';
+                $this->view('templates/header', $data);
+
+                $this->view('templates/navbar/main-navbar');
+
+                $data['username'] = $_SESSION['username'];
+                $this->view('dashboard/index', $data);
+
+                $this->view('templates/footer');
+            }
+        }
     }
 }
