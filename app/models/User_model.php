c3a59f4187104d29bd3f3065092aecb44a6d4785
dev4ult <nibrasbiasaaja@gmail.com>
2022-12-12 20:40:53 +0700

    add functionality to autodelete after insert and send otp code for new user

--- app/models/User_model.php
+++ app/models/User_model.php
@@ -2,6 +2,7 @@
 
 // sent to user email for verifying the new user
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 // email validator to validate email
@@ -24,7 +25,7 @@ class User_model {
         return $this->username;
     }
 
-    public function isEmailExist($email) {
+    public function isEmailRegistered($email) {
         $this->db->query("SELECT * FROM users WHERE email=:email");
         $this->db->bind('email', $email);
         return $this->db->single();
@@ -32,34 +33,40 @@ class User_model {
 
     public function sendEmailForVerify($email, $name, $otp_code) {
         try {
-
             $mail = new PHPMailer(true);
 
             $mail->isSMTP();
 
-            $mail->Host = '-';
+            $mail->Host = EMAIL_HOST;
             $mail->SMTPAuth = true;
-            $mail->Username = '-';
-            $mail->Password = '-';
+            $mail->Username = EMAIL;
+            $mail->Password = EMAIL_APP_PASS;
 
-            $mail->SMTPSecure = "ssl";
-            $mail->Port = '-';
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+            $mail->Port = EMAIL_PORT;
 
-            $mail->setFrom('proctelearn@gmail.com');
+            $mail->setFrom(EMAIL, EMAIL_FROM);
 
             //Recipients
             $mail->addAddress($email, $name);
 
             $mail->isHTML(true);
-            $mail->Subject = 'Verify your Account';
-            $html_draft = "<h3>Verify Your Email | ProcteLearn</h3>
-                        <p>OTP CODE : $otp_code</p>";
-            $mail->Body = $html_draft;
+            $mail->Subject = 'Your account need to be verified first';
+            $mail->Body = "<h2>OTP Code</h2><h3 style='padding: 1.25rem; font-size: 2rem; color: white; background-color: black; width: fit-content;'>$otp_code</h3>";
+            $mail->AltBody = "OTP Code : $otp_code";
 
             $mail->send();
         } catch (Exception $e) {
-            Flasher::setFlash("Message could not be sent. Error : {$mail->ErrorInfo}");
+            Flasher::setFlash("OTP code could not be sent");
         }
+    }
+
+    public function delRowInPeriod($email) {
+        $this->db->query("CREATE EVENT delete_row 
+                        ON SCHEDULE AT CURRENT_TIMESTAMP + INTERVAL 20 Second
+                        DO DELETE FROM unreg_users WHERE email=:email");
+        $this->db->bind('email', $email);
+        $this->db->execute();
     }
 
     public function addNewUser($data) {
@@ -69,8 +76,14 @@ class User_model {
         $passConf = htmlspecialchars($data['password-confirmation']);
         $otp_code = $data['otp-code'];
 
+        // check post data
+        if ($username == '' || $email == '') {
+            Flasher::setFlash('Username or email can not be empty', 'error');
+            return -1;
+        }
+
         // check if email is already been used
-        if ($this->isEmailExist($email)) {
+        if ($this->isEmailRegistered($email)) {
             Flasher::setFlash('That email has already been used', 'error');
             return -1;
         }
@@ -88,8 +101,8 @@ class User_model {
         $this->db->bind('email', $email);
         $this->db->bind('password', hash('sha256', $password));
         $this->db->bind('otp_code', $otp_code);
-
         $this->db->execute();
+
         return $this->db->rowChangeCheck();
     }
 
