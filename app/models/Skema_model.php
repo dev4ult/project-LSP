d804dd328dd7eeae609094ed53b6a8fcec98dd5d
FaresAriandha <AkunKoding2009@gmail.com>
2023-01-07 23:59:28 +0700

    Tambahin search skema dan ambil jumlah asesi terdaftar dan telah asesmen

--- app/models/Skema_model.php
+++ app/models/Skema_model.php
@@ -9,7 +9,7 @@ class Skema_model
 
   public function fetchAllSkema($page)
   {
-    $query = "SELECT skema_sertifikasi.id, nama_skema, skkni.skkni, status, level FROM skema_sertifikasi JOIN skkni ON skema_sertifikasi.id_skkni = skkni.id ORDER BY skema_sertifikasi.id ASC LIMIT :page, 5";
+    $query = "SELECT skema_sertifikasi.id, nama_skema, skkni, status, level FROM skema_sertifikasi ORDER BY skema_sertifikasi.id ASC LIMIT :page, 5";
     $this->db->query($query);
     $this->db->bind("page", 5 * ($page - 1));
     return $this->db->resultSet();
@@ -17,14 +17,19 @@ class Skema_model
 
   public function addSkema($data)
   {
-    $idSkkni = $this->getIdSKKNNI($data['SKKNI'])['id'];
-    $query = "INSERT INTO skema_sertifikasi(id, nama_skema, id_skkni, status, level) VALUES ('', :nama, :idSkkni, 'Aktif', :level)";
+    // $idSkkni = $this->getIdSKKNI($data['SKKNI'])['id'];
+    $skema = htmlspecialchars($data['skema']);
+    $query = "INSERT INTO skema_sertifikasi(id, nama_skema, skkni, status, level) VALUES ('', :nama, :skkni, 'Aktif', :level)";
     $this->db->query($query);
-    $this->db->bind("nama", $data['skema']);
-    $this->db->bind("idSkkni", $idSkkni);
+    $this->db->bind("nama", $skema);
+    $this->db->bind("skkni", $data['SKKNI']);
     $this->db->bind("level", $data['level']);
-    $this->db->execute();
-    return $this->db->rowChangeCheck();
+    try {
+      $this->db->execute();
+      return $this->db->rowChangeCheck();
+    } catch (\Throwable $th) {
+      Flasher::setFlash("Harap ganti level dari skemanya", 'warning');
+    }
   }
 
   public function updateStatus($id, $status)
@@ -39,7 +44,7 @@ class Skema_model
 
   public function getSingleSkema($id)
   {
-    $query = "SELECT skema_sertifikasi.id as id_skema, nama_skema, skkni.skkni as skkni, level FROM skema_sertifikasi JOIN skkni on skema_sertifikasi.id_skkni = skkni.id WHERE skema_sertifikasi.id=:id";
+    $query = "SELECT skema_sertifikasi.id as id_skema, nama_skema, skkni, level FROM skema_sertifikasi WHERE skema_sertifikasi.id=:id";
     $this->db->query($query);
     $this->db->bind("id", $id);
     return $this->db->single();
@@ -52,24 +57,59 @@ class Skema_model
     return $this->db->resultSet();
   }
 
-  public function getIdSKKNNI($skkni)
+  public function getIdSKKNI($skkni)
   {
-    $query = "SELECT id FROM skkni WHERE skkni = :skkni";
+    $query = "SELECT id FROM skkni WHERE skkni LIKE ':skkni'";
     $this->db->query($query);
-    $this->db->bind("skkni", $skkni);
+    $this->db->bind("%skkni%", $skkni);
     return $this->db->single();
   }
 
   public function updateSkema($id, $data)
   {
-    $idSkkni = $this->getIdSKKNNI($data['SKKNI'])['id'];
-    $query = "UPDATE skema_sertifikasi SET nama_skema=:nama, id_skkni=:id_skkni, level=:level WHERE id=:id";
+    // $idSkkni = $this->getIdSKKNI($data['SKKNI'])['id'];
+    $skema = htmlspecialchars($data['skema']);
+    $query = "UPDATE skema_sertifikasi SET nama_skema=:nama, skkni=:skkni, level=:level WHERE id=:id";
     $this->db->query($query);
     $this->db->bind("id", $id);
-    $this->db->bind("nama", $data['skema']);
-    $this->db->bind("id_skkni", $idSkkni);
+    $this->db->bind("nama", $skema);
+    $this->db->bind("skkni", $data['SKKNI']);
     $this->db->bind("level", $data['level']);
-    $this->db->execute();
-    return $this->db->rowChangeCheck();
+    try {
+      $this->db->execute();
+      return $this->db->rowChangeCheck();
+    } catch (\Throwable $th) {
+      Flasher::setFlash("Harap ganti level dari skemanya", 'warning');
+    }
+  }
+
+  public function searchData($page, $keyword)
+  {
+    // $idSkkni = $this->getIdSKKNI($keyword)['id'];
+
+    $query = "SELECT skema_sertifikasi.id, nama_skema, skkni, status, level FROM skema_sertifikasi WHERE nama_skema LIKE :word OR skkni LIKE :word OR status LIKE :word ORDER BY skema_sertifikasi.id ASC LIMIT :page, 5";
+    $this->db->query($query);
+    $this->db->bind("word", "%" . $keyword . "%");
+    // $this->db->bind("'%status%'", $keyword);
+    // $this->db->bind("'%skkni%'", $keyword);
+    $this->db->bind("page", 5 * ($page - 1));
+    return $this->db->resultSet();
+  }
+
+  public function countAsesiRegistered($id)
+  {
+    $query = "SELECT COUNT(*) as jumlah from daftar_asesi_sertifikasi where id_skema_sertifikasi =:id";
+    $this->db->query($query);
+    $this->db->bind("id", $id);
+    return $this->db->single();
+  }
+
+  public function countAsesiAssessed($id)
+  {
+    $query = "SELECT COUNT(*) as jumlah from daftar_asesi_sertifikasi WHERE id_skema_sertifikasi =:id AND status_asesmen =:status";
+    $this->db->query($query);
+    $this->db->bind("id", $id);
+    $this->db->bind("status", "Sudah");
+    return $this->db->single();
   }
 }
