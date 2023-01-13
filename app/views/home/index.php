8179c625d4604c532282dcd8971b07b8ff658f4a
kyasa422 <kyasa422@gmail.com>
2023-01-13 22:00:57 +0700

    homepage FE

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,10 +1,104 @@
-<div class="hero min-h-screen bg-base-200">
-    <div class="hero-content text-center">
-        <div class="max-w-md">
-            <h1 class="text-5xl font-bold">Hello there</h1>
-            <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <a href="<?= BASEURL ?>/registration" class="btn btn-primary">Get Started</a>
+<!-- <div class="hero min-h-screen bg-base-200">
+    
+    <div class="hero-content text-left">
+        <div class="max-w-md">      
+                   
+           
+        </div>
+        
+    </div>
+    
+
+    
+</div>   -->
+
+<div class="bg-base-200 ">
+
+
+    <div>
+        <p class="text-5xl font-bold-sm mb-6 pt-6">SELAMAT DATANG </p>
+        <p class="mb-6 ml-3">Politeknik Negri Jakarta</p>
+        <p class="mb-6">Peserta Uji Kompetensi</p>
+        <p class="mb-6">Asesor</p>
+        <p class="mb-6">Manajemen</p>
+
+    </div>
+
+    <!-- info grafis -->
+    <div>
+        <h3 class="text-center mt-5"> Infografis</h3>
+        <h1 class="text-center mt-5 text-3xl font-bold"> Statistics LSP</h1>
+        <p class="text-center"> pencapaian LSP DEMO dalam angka</p>
+        <div class="flex items-stretch text-white mb-4">
+
+            <div class="w-1/4 text-center bg-slate-700 rounded-full ">
+                <p>32</p>
+                Asesor
+            </div>
+            <div class="w-1/4 text-center  bg-slate-700 rounded-full">
+                <p>32</p>
+                Asesor
+            </div>
+            <div class="w-1/4 text-center  bg-slate-700 rounded-full">
+                <p>32</p>
+                Asesor
+            </div>
+            <div class="w-1/4 text-center  bg-slate-700 rounded-full">
+                <p>32</p>
+                Asesor
+            </div>
+
+        </div>
+
+    </div>
+
+    <a href="<?= BASEURL ?>/registration" class="btn btn-primary">Hubungi Kami</a>
+
+    <!-- info terkini -->
+    <div class="flex  ">
+        <h1 class="mt-5 text-2xl font-bold"> INFO TERKINI</h1>
+        <h1 class="text-end mt-5 text-2xl font-bold ml-auto mr-5">JADWAL ASESMEN TERKINI </h1>
+
+    </div>
+
+    <div class="flex bg-base-200 gap-3">
+
+        <div class="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+            <div class="card-body">
+                <h2 class="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
+
+
+
+
+        <div class="card card-compact w-96 bg-base-100 shadow-xl ">
+            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+            <div class="card-body">
+                <h2 class="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="card w-96 bg-slate-500 shadow-xl">
+            <div class="card-body">
+
+                <p>Agenda/ jadwal penyelenggaraan uji kompetensi LSP DEMO yang dapat anda ikuti dapat dilihat pada data sebagai berikut.</p>
+                <div class="card-actions justify-end">
+                </div>
+
+            </div>
+
+
         </div>
     </div>
+
 </div>
